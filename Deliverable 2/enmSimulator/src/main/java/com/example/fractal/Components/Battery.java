247b4655d408afafb8971455b30d1474a4a91712
vass57 <vassiakoutsoukos@gmail.com>
2025-02-17 18:57:57 -0500

    Battery
    Worked on the visual part of the battery as well as the polarity

--- Deliverable 2/enmSimulator/src/main/java/com/example/fractal/Components/Battery.java
+++ Deliverable 2/enmSimulator/src/main/java/com/example/fractal/Components/Battery.java
@@ -3,11 +3,13 @@ package com.example.fractal.Components;
 import com.example.fractal.Component;
 import com.example.fractal.Node;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Battery extends Component {
-    private double potential;  // The potential difference (voltage) of the battery
-    private boolean startPolarity; // The positive side (start) of the battery
-    private boolean endPolarity;  // The negative side (end) of the battery
+    private double potential;  
+    private boolean startPolarity; 
+    private boolean endPolarity;  
+    private ImageView batteryImageView;  
 
     // Constructor with a potential value and polarity settings for start and end
     public Battery(Node begin, Node end, double potential) {
@@ -15,6 +17,14 @@ public class Battery extends Component {
         this.potential = potential;
         this.startPolarity = true;  // Default polarity for the start
         this.endPolarity = false;   // Default polarity for the end
+
+        
+        Image batteryImage = new Image(getClass().getResource("/com/example/fractal/images/Batterysymbol.png").toExternalForm());
+        this.batteryImageView = new ImageView(batteryImage);
+
+      
+        this.batteryImageView.setX(begin.getX());
+        this.batteryImageView.setY(begin.getY());
     }
 
     // Getter for potential (voltage)
@@ -42,14 +52,38 @@ public class Battery extends Component {
         this.endPolarity = polarity;
     }
 
-    @Override
-    public Image getSymbol() {
-        return DIAGRAM_DISPLAY;  // Use the symbol image for the battery
+    // Swap the start and end polarities
+    public void swapPolarity() {
+        boolean temp = startPolarity;
+        startPolarity = endPolarity;
+        endPolarity = temp;
     }
 
+    
+    @Override
+    public Image getSymbol() {
+        return batteryImageView.getImage();
+    }
+
+   
     @Override
     public void draw() {
         System.out.println("Battery from " + begin + " to " + end);
-        // Add drawing logic if needed, e.g., rendering a battery symbol
+        
+        // Calculate the angle between the begin and end nodes
+        double deltaX = end.getX() - begin.getX();
+        double deltaY = end.getY() - begin.getY();
+        double angle = Math.toDegrees(Math.atan2(deltaY, deltaX));
+
+        // Set the rotation of the battery image based on the angle
+        batteryImageView.setRotate(angle);
+        // Update the position of the battery based on the start node
+        batteryImageView.setX(begin.getX());
+        batteryImageView.setY(begin.getY());
+    }
+
+    // Get the ImageView for the battery so that it can be added to the DrawingArea
+    public ImageView getImageView() {
+        return batteryImageView;
     }
 }
