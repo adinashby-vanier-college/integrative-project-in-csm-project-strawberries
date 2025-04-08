db1f04f3093999d19e7f4398fefad5df2fcb47a3
eos <tibahammoudi@gmail.com>
2025-04-07 22:17:02 -0400

    implemented drag and rotate logic for the switch ( open )

--- src/main/java/edu/vanier/strawberries/Components/Switch.java
+++ src/main/java/edu/vanier/strawberries/Components/Switch.java
@@ -1,13 +1,33 @@
 package edu.vanier.strawberries.Components;
 import edu.vanier.strawberries.Component;
 import edu.vanier.strawberries.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+
+import java.net.URL;
 
 public class Switch extends Component {
     private boolean isClosed;
+    private double mouseOffsetX;
+    private double mouseOffsetY;
+    private final ImageView openswitchImageView;
 
     public Switch(Node begin, Node end, boolean isClosed) {
         super(begin, end);
         this.isClosed = isClosed;
+
+        URL imgUrl = getClass().getResource("/images/open_switch_diagram.png");
+        if (imgUrl == null) {
+            System.out.println("Could not load open switch image");
+        }
+        Image openSwitchImage = new Image(imgUrl.toExternalForm());
+        DIAGRAM_DISPLAY = openSwitchImage;
+        openswitchImageView = new ImageView(openSwitchImage);
+        openswitchImageView.setFitWidth(100);
+        openswitchImageView.setPreserveRatio(true);
+        getChildren().add(openswitchImageView);
+        enableDragAndRotate();
     }
 
     public void toggle() {
@@ -17,9 +37,56 @@ public class Switch extends Component {
     public void setOpen(boolean open) {
        //
     }
-
-    @Override
-    public void draw() {
-        System.out.println("Switch (" + (isClosed ? "Closed" : "Open") + ") from " + begin + " to " + end);
+    public ImageView getImageView() {
+        return openswitchImageView;
     }
+
+    public void enableDragAndRotate() {
+        this.setOnMousePressed(e -> { // stores information as to where the mouse is from the image's corner
+            mouseOffsetX = e.getSceneX() - this.getLayoutX();
+            mouseOffsetY = e.getSceneY() - this.getLayoutY();
+            e.consume();
+        });
+
+    this.setOnMouseDragged(e -> { // calculates angle between components center and the mouse location
+        if (e.isSecondaryButtonDown()) { // the right mouse button
+            double centerX = this.getLayoutX() + this.getBoundsInParent().getWidth() / 2;
+            double centerY = this.getLayoutY() + this.getBoundsInParent().getHeight() / 2;
+            double angle = Math.toDegrees(Math.atan2(e.getSceneY() - centerY, e.getSceneX() - centerX));
+            this.setRotate(angle); // rotates using trig
+        } else {
+            double newX = e.getSceneX() - mouseOffsetX;
+            double newY = e.getSceneY() - mouseOffsetY;
+            this.setLayoutX(newX);
+            this.setLayoutY(newY);
+            //  update the logical node positions too
+            this.begin.setPosition(newX, newY);
+            // keep end node relative to the original angle/distance
+            double deltaX = end.getX() - begin.getX();
+            double deltaY = end.getY() - begin.getY();
+            this.end.setPosition(newX + deltaX, newY + deltaY);
+
+        }
+        e.consume(); // basically makes it so that only the battery moves
+    });
+}
+
+    public Image getSymbol() {
+        return openswitchImageView.getImage();
+    }
+    public void draw() {
+        double deltaX = end.getX() - begin.getX();
+        double deltaY = end.getY() - begin.getY();
+        double angle = Math.toDegrees(Math.atan2(deltaY, deltaX));
+
+        Rotate rotation = getAngleRotate();
+
+        // Set the rotation of the battery image based on the angle
+
+        display.getTransforms().clear();
+        display.getTransforms().add(rotation);
+        setLayoutX(begin.getX());
+        setLayoutY(begin.getY());
+    }
+
 }
