f68d45d9b7db010fad47ad6c03557118199a3726
vass57 <107373099+vass57@users.noreply.github.com>
2025-05-05 18:52:23 -0400

    fixed lightbulb logic and ui

--- src/main/java/edu/vanier/strawberries/Models/DrawingArea.java
+++ src/main/java/edu/vanier/strawberries/Models/DrawingArea.java
@@ -21,6 +21,7 @@ import java.util.*;
 import javafx.scene.paint.Color;
 
 public class DrawingArea {
+
     public DrawingTool drawingTool = new DrawingTool();
     private Component selection;
     public Circuit circuit;
@@ -44,17 +45,21 @@ public class DrawingArea {
     }
 
     /**
-     * Method called every frame. Draws the elements to be displayed in the canvas, such as circuit components
+     * Method called every frame. Draws the elements to be displayed in the
+     * canvas, such as circuit components
      */
     public void drawContent() {
-        gc.clearRect(0,0,canvas.getWidth(),canvas.getHeight());
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
         // Draw the grid
-        if(showGrid) {
-            switch(MainApp.mainAppFXMLController.getTheme()) {
-                case "light-mode" -> gc.setStroke(Color.LIGHTGREY);
-                case "dark-mode" -> gc.setStroke(Color.BLACK);
-                case "strawberries-theme" -> gc.setStroke(Color.PINK);
+        if (showGrid) {
+            switch (MainApp.mainAppFXMLController.getTheme()) {
+                case "light-mode" ->
+                    gc.setStroke(Color.LIGHTGREY);
+                case "dark-mode" ->
+                    gc.setStroke(Color.BLACK);
+                case "strawberries-theme" ->
+                    gc.setStroke(Color.PINK);
             }
             gc.setLineWidth(1);
             for (int i = 0; i < canvas.getWidth(); i += (int) (squareSize * zoom)) {
@@ -73,70 +78,79 @@ public class DrawingArea {
         }
 
         // Draw the Components
-        for(Component component : circuit.toArrayList()) {
+        for (Component component : circuit.toArrayList()) {
             assert component != null;
 
-            if(component instanceof Wire wire) {
+            if (component instanceof Wire wire) {
                 gc.setLineWidth(3);
                 gc.setStroke(wire.getColor());
-                gc.strokeLine(component.begin.getX(),component.begin.getY(),component.end.getX(),component.end.getY());
-                Node[] nodes = {wire.begin,wire.end};
-                for(Node node : nodes) {
-                    if(node.isConnected()) {
+                gc.strokeLine(component.begin.getX(), component.begin.getY(), component.end.getX(), component.end.getY());
+                Node[] nodes = {wire.begin, wire.end};
+                for (Node node : nodes) {
+                    if (node.isConnected()) {
                         gc.setFill(wire.getColor());
-                        gc.fillOval(node.getX()-4,node.getY()-4,8,8);
+                        gc.fillOval(node.getX() - 4, node.getY() - 4, 8, 8);
                     }
                 }
-                if(wire.selected) {
+                if (wire.selected) {
                     gc.setFill(Color.BLACK);
-                    gc.fillOval(wire.begin.getX()-5,wire.begin.getY()-5,10,10);
-                    gc.fillOval(wire.end.getX()-5,wire.end.getY()-5,10,10);
+                    gc.fillOval(wire.begin.getX() - 5, wire.begin.getY() - 5, 10, 10);
+                    gc.fillOval(wire.end.getX() - 5, wire.end.getY() - 5, 10, 10);
                 }
-            }
-            else {
+            } else {
                 Image img = component.display;
-                Rotate rotateTransform = new Rotate(component.getAngle(),component.begin.getX(),component.begin.getY());
+                Rotate rotateTransform = new Rotate(component.getAngle(), component.begin.getX(), component.begin.getY());
                 gc.save();
                 gc.setTransform(new Affine(rotateTransform));
-                gc.drawImage(img,component.begin.getX(),component.begin.getY()-(img.getHeight())/2,img.getWidth()*zoom,img.getHeight()*zoom);
+                gc.drawImage(img, component.begin.getX(), component.begin.getY() - (img.getHeight()) / 2, img.getWidth() * zoom, img.getHeight() * zoom);
 
-                if(component.isEdit()) {
+                if (component.isEdit()) {
                     // SHOW IF THE COMPONENT IS BEING EDITED
                     gc.setStroke(Color.BLUE);
                     gc.setLineWidth(1.2);
-                    gc.strokeRect(component.begin.getX(),component.begin.getY()-(img.getHeight())/2,img.getWidth()*zoom,img.getHeight()*zoom);
+                    gc.strokeRect(component.begin.getX(), component.begin.getY() - (img.getHeight()) / 2, img.getWidth() * zoom, img.getHeight() * zoom);
                 }
                 gc.restore();
 
-                if(component instanceof Lightbulb lightbulb && lightbulb.isOn()) {
-                    //TODO set opacity based on lightbulb's current voltage
-                    //TODO not working for rotation
-                    gc.setFill(lightbulb.getColor().deriveColor(0,1,1, 0.5));
-                    gc.fillOval(lightbulb.getCenter().getX()-20,lightbulb.getCenter().getY()-20,40,40);
+                if (component instanceof Lightbulb lightbulb && lightbulb.isOn()) {
+                    double haloX = lightbulb.getCenter().getX() - 20;
+                    double haloY = lightbulb.getCenter().getY() - 20;
+
+                    if (!lightbulb.diagramView) {
+                        haloY -= 50;
+                    }
+
+                    gc.setFill(lightbulb.getColor().deriveColor(0, 1, 1, 0.5));
+                    gc.fillOval(haloX, haloY, 40, 40);
                 }
+
             }
         }
     }
 
     /**
-     * Allows the component nodes to snap to the closed grid intersection when dragging
+     * Allows the component nodes to snap to the closed grid intersection when
+     * dragging
+     *
      * @param pos the un-snapped position of the node
      * @return The updated, snapped position relative to the grid.
      */
     public double snap(double pos) {
         double remainder = pos % (squareSize);
-        if (remainder == 0)
+        if (remainder == 0) {
             return pos;
-        else if (remainder <= (double) squareSize / 2)
+        } else if (remainder <= (double) squareSize / 2) {
             return pos - remainder;
-        else
+        } else {
             return pos + (squareSize - remainder);
+        }
     }
 
     /**
-     *  INSERT JAVADOC COMMENT HERE
+     * INSERT JAVADOC COMMENT HERE
      */
     private static class Electron {
+
         private final Wire wire;
         private double progress;
         private final double speedFactor;
@@ -163,7 +177,6 @@ public class DrawingArea {
         }
     }
 
-
     public String exportCircuit(String circuitName) {
         StringBuilder output;
         if (circuitName != null) {
@@ -172,29 +185,31 @@ public class DrawingArea {
             output = new StringBuilder("unnamedCircuit.txt\n");
         }
         // go through each component and add to final output
-        for(Component component : circuit.toArrayList()) {
+        for (Component component : circuit.toArrayList()) {
             assert component != null;
             if (component instanceof Wire wire) {
                 Node[] nodes = {wire.begin, wire.end};
                 for (Node node : nodes) {
-                    if (node.isConnected()) output.append("1|");
+                    if (node.isConnected()) {
+                        output.append("1|");
+                    }
                 }
                 gc.setLineWidth(3);
                 output.append("wire|").append(wire.getColor()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.end.getX()).append("|").append(component.end.getY()).append("|").append(component.getCurrent()).append("|").append(component.getVoltage()).append("\n");
             } else {
                 switch (component) {
                     case Resistor resistor ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(resistor.getResistance()).append("\n");
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(resistor.getResistance()).append("\n");
                     case Battery battery ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(battery.getPotential()).append("\n");
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(battery.getPotential()).append("\n");
                     case Capacitor capacitor ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(capacitor.getStoredEnergy()).append("\n");
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(capacitor.getStoredEnergy()).append("\n");
                     case Lightbulb lightbulb ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(lightbulb.getResistance()).append("\n");
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(lightbulb.getResistance()).append("\n");
                     case Fuse fuse ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(fuse.getMaxCurrent()).append("\n");
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(fuse.getMaxCurrent()).append("\n");
                     case Switch switchh ->
-                            output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(switchh.getCurrent()).append("\n"); // curent not used, but it is here to not break formatting
+                        output.append(component.getType()).append("|").append(component.begin.getX()).append("|").append(component.begin.getY()).append("|").append(component.getAngle()).append("|").append(switchh.getCurrent()).append("\n"); // curent not used, but it is here to not break formatting
                     default -> {
                     }
                 }
@@ -202,7 +217,7 @@ public class DrawingArea {
         }
         // remove duplicate lines
         StringBuilder builder = new StringBuilder();
-        for (String line: new LinkedHashSet<String>(Arrays.asList(output.toString().split("\n"))) ) {
+        for (String line : new LinkedHashSet<String>(Arrays.asList(output.toString().split("\n")))) {
             builder.append(line).append("\n");
         }
         String result = builder.toString();
@@ -210,79 +225,78 @@ public class DrawingArea {
         return result;
     }
 
-
     boolean printedValues = false;
 
     public void animateCurrentFlow(boolean start) {
-    animateCurrent = start;
-    activeTransitions.clear();
-    animatedElectrons.clear();
+        animateCurrent = start;
+        activeTransitions.clear();
+        animatedElectrons.clear();
 
-    if (!(canvas.getParent() instanceof Pane parent)) return;
-
-    if (start) {
-        int dotsPerWire = 5;
-        double speedFactor;
-
-        CircuitMath circuitMath = new CircuitMath(circuit);
-        double totalCurrent = circuitMath.getTotalCurrent(); // get total current
-        double totalResistance = circuitMath.getTotalResistance();
-        double totalVoltage = circuitMath.getTotalVoltage();
-
-        if (!printedValues) {
-            System.out.println("Voltage: " + totalVoltage + "V");
-            System.out.println("Resistance: " + totalResistance + "Ω");
-            System.out.println("Current: " + totalCurrent + "A");
-            printedValues = true;
+        if (!(canvas.getParent() instanceof Pane parent)) {
+            return;
         }
 
-     
-        for (LinkedList<Component> list : circuit.arrayList) {
-            for (Component c : list) {
-                if (c instanceof Fuse fuse) {
-                    if (totalCurrent > fuse.getMaxCurrent()) {
-                        fuse.updateState(totalCurrent); 
-                        animateCurrent = false;
+        if (start) {
+            int dotsPerWire = 5;
+            double speedFactor;
 
-                        
-                        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
-                        alert.setTitle("Fuse Blown");
-                        alert.setHeaderText("Current exceeded fuse limit!");
-                        alert.setContentText("Current: " + totalCurrent + " A\nLimit: " + fuse.getMaxCurrent() + " A\nThe fuse has blown.");
-                        alert.showAndWait();
+            CircuitMath circuitMath = new CircuitMath(circuit);
+            double totalCurrent = circuitMath.getTotalCurrent(); // get total current
+            double totalResistance = circuitMath.getTotalResistance();
+            double totalVoltage = circuitMath.getTotalVoltage();
 
-                        return; 
+            if (!printedValues) {
+                System.out.println("Voltage: " + totalVoltage + "V");
+                System.out.println("Resistance: " + totalResistance + "Ω");
+                System.out.println("Current: " + totalCurrent + "A");
+                printedValues = true;
+            }
+
+            for (LinkedList<Component> list : circuit.arrayList) {
+                for (Component c : list) {
+                    if (c instanceof Fuse fuse) {
+                        if (totalCurrent > fuse.getMaxCurrent()) {
+                            fuse.updateState(totalCurrent);
+                            animateCurrent = false;
+
+                            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
+                            alert.setTitle("Fuse Blown");
+                            alert.setHeaderText("Current exceeded fuse limit!");
+                            alert.setContentText("Current: " + totalCurrent + " A\nLimit: " + fuse.getMaxCurrent() + " A\nThe fuse has blown.");
+                            alert.showAndWait();
+
+                            return;
+                        }
                     }
                 }
             }
-        }
 
-      
-        if (totalCurrent < 0.5) {
-            speedFactor = 500;
-        } else if (totalCurrent < 2 && totalCurrent > 0.5) {
-            speedFactor = 1000.0;
-        } else {
-            speedFactor = 10000.0;
-        }
+            if (totalCurrent < 0.5) {
+                speedFactor = 500;
+            } else if (totalCurrent < 2 && totalCurrent > 0.5) {
+                speedFactor = 1000.0;
+            } else {
+                speedFactor = 10000.0;
+            }
 
-        for (LinkedList<Component> list : circuit.arrayList) {
-            for (Component c : list) {
-                if (c instanceof Wire wire) {
-                    for (int i = 0; i < dotsPerWire; i++) {
-                        Electron e = new Electron(wire, speedFactor);
-                        e.progress = i / (double) dotsPerWire;
-                        animatedElectrons.add(e);
+            for (LinkedList<Component> list : circuit.arrayList) {
+                for (Component c : list) {
+                    if (c instanceof Wire wire) {
+                        for (int i = 0; i < dotsPerWire; i++) {
+                            Electron e = new Electron(wire, speedFactor);
+                            e.progress = i / (double) dotsPerWire;
+                            animatedElectrons.add(e);
+                        }
                     }
                 }
             }
         }
     }
-}
-
 
     public void stopElectronAnimation() {
-        if (!(canvas.getParent() instanceof Pane parent)) return;
+        if (!(canvas.getParent() instanceof Pane parent)) {
+            return;
+        }
 
         for (PathTransition transition : activeTransitions) {
             transition.stop();
@@ -297,11 +311,15 @@ public class DrawingArea {
     }
 
     public void zoomOut() {
-        if(zoom > 0.1) setZoom(zoom - 0.1);
+        if (zoom > 0.1) {
+            setZoom(zoom - 0.1);
+        }
     }
 
     public void zoomIn() {
-        if(zoom < 2) setZoom(zoom + 0.1);
+        if (zoom < 2) {
+            setZoom(zoom + 0.1);
+        }
     }
 
     public double getZoom() {
@@ -321,7 +339,6 @@ public class DrawingArea {
             // Get the total current from CircuitMath
             CircuitMath circuitMath = new CircuitMath(circuit);
             double totalCurrent = circuitMath.getTotalCurrent();  // Get the total current
-
 
             // Update all electrons with the current speed factor
             for (Electron e : animatedElectrons) {
--- src/main/java/edu/vanier/strawberries/Models/Lightbulb.java
+++ src/main/java/edu/vanier/strawberries/Models/Lightbulb.java
@@ -11,20 +11,20 @@ public class Lightbulb extends Component {
     private Color color;
 
     // Shared image references
-    private static final Image OFF_IMAGE;
-    private static final Image YELLOW_ON, RED_ON, BLUE_ON, GREEN_ON;
-    private static final Image DIAGRAM_IMAGE, REALISTIC_IMAGE;
-
+    private static final Image DIAGRAM_OFF_IMAGE, REALISTIC_OFF_IMAGE;
+    private static final Image DIAGRAM_YELLOW_ON, DIAGRAM_RED_ON, DIAGRAM_BLUE_ON, DIAGRAM_GREEN_ON;
+  
     static {
         try {
-            OFF_IMAGE = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_real.png")).toExternalForm());
-            YELLOW_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_yellow.png")).toExternalForm());
-            RED_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_red.png")).toExternalForm());
-            BLUE_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_blue.png")).toExternalForm());
-            GREEN_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_green.png")).toExternalForm());
+            DIAGRAM_OFF_IMAGE = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram.png")).toExternalForm());
+            REALISTIC_OFF_IMAGE = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_real.png")).toExternalForm());
 
-            DIAGRAM_IMAGE = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram.png")).toExternalForm());
-            REALISTIC_IMAGE = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_real.png")).toExternalForm());
+            DIAGRAM_YELLOW_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_yellow.png")).toExternalForm());
+            DIAGRAM_RED_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_red.png")).toExternalForm());
+            DIAGRAM_BLUE_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_blue.png")).toExternalForm());
+            DIAGRAM_GREEN_ON = new Image(Objects.requireNonNull(Lightbulb.class.getResource("/images/lightbulb_diagram_green.png")).toExternalForm());
+
+
         } catch (Exception e) {
             throw new RuntimeException("Failed to load lightbulb images.");
         }
@@ -35,8 +35,8 @@ public class Lightbulb extends Component {
         this.color = color;
         this.minVoltage = 10;
 
-        this.DIAGRAM_DISPLAY = DIAGRAM_IMAGE;
-        this.IMAGE_DISPLAY = OFF_IMAGE;
+        this.DIAGRAM_DISPLAY = DIAGRAM_OFF_IMAGE;
+        this.IMAGE_DISPLAY = REALISTIC_OFF_IMAGE;
         this.display = diagramView ? DIAGRAM_DISPLAY : IMAGE_DISPLAY;
     }
 
@@ -52,19 +52,16 @@ public class Lightbulb extends Component {
         this.on = on;
 
         if (!on) {
-            display = OFF_IMAGE;
+            display = diagramView ? DIAGRAM_OFF_IMAGE : REALISTIC_OFF_IMAGE;
         } else {
-            if (color.equals(Color.YELLOW)) {
-                display = YELLOW_ON;
-            } else if (color.equals(Color.RED)) {
-                display = RED_ON;
-            } else if (color.equals(Color.BLUE)) {
-                display = BLUE_ON;
-            } else if (color.equals(Color.GREEN)) {
-                display = GREEN_ON;
-            } else {
-                display = OFF_IMAGE; // fallback
-            }
+            if (diagramView) {
+                if (color.equals(Color.YELLOW)) display = DIAGRAM_YELLOW_ON;
+                else if (color.equals(Color.RED)) display = DIAGRAM_RED_ON;
+                else if (color.equals(Color.BLUE)) display = DIAGRAM_BLUE_ON;
+                else if (color.equals(Color.GREEN)) display = DIAGRAM_GREEN_ON;
+                else display = DIAGRAM_OFF_IMAGE;
+          } 
+  
         }
     }
 
